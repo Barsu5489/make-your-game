72c8e73a5db7acbd0391c18e8aab580de06edfd6
ebarsula <ebarsulai@gmail.com>
2025-01-29 13:20:07 +0300

    Refactor: Move Player class to its own file player.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import { BulletPool } from "./bulletpull.js";
+import { Player } from "./player.js";
 class Game {
   constructor() {
     this.container = document.getElementById('gameContainer');
@@ -436,30 +437,7 @@ levelUp() {
 document.addEventListener('DOMContentLoaded', () => {
   const game = new Game();
 });
-class Player {
-  constructor(container) {
-    this.element = document.createElement('div');
-    this.element.className = 'player';
-    this.x = 380; // Center position (800 - playerWidth) / 2
-    this.speed = 0.3;
-    this.container = container;
-    this.width = 30; 
-    this.init();
-  }
 
-  init() {
-    this.container.appendChild(this.element);
-    this.element.style.transform = `translateX(${this.x}px)`; // Initial position
-  }
-
-  move(direction, deltaTime) {
-    this.x += direction * this.speed * deltaTime;
-    // Update boundary check to account for player width
-    const maxX = this.container.offsetWidth - this.width;
-    this.x = Math.max(0, Math.min(maxX, this.x));
-    this.element.style.transform = `translateX(${this.x}px)`;
-  }
-}
 class Enemy {
   constructor(container, x, y, row, col) {
     this.element = document.createElement('div');
